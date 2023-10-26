f49c5e811cc198718948544835642c0108266b35
João de Vasconcelos <johnyvasconcelos@icloud.com>
2023-10-26 22:31:23 +0100

    Update download.endpoint.js

--- server/endpoints/download.endpoint.js
+++ server/endpoints/download.endpoint.js
@@ -9,6 +9,7 @@ module.exports = async (request, reply) => {
 
   // Search database for requested job
   const foundRequestedJob = await QUEUEDB.Job.findOne({ _id: request.params.id });
+  console.log('request.params.id', request.params.id);
   console.log('foundRequestedJob', foundRequestedJob);
 
   // If no job is found with this id return 404 Not Found
