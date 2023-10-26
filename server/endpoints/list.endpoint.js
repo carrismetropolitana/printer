09274527979619c062b76b48fca2439e53002f63
João de Vasconcelos <johnyvasconcelos@icloud.com>
2023-10-26 22:22:15 +0100

    Update list.endpoint.js

--- server/endpoints/list.endpoint.js
+++ server/endpoints/list.endpoint.js
@@ -7,8 +7,7 @@ const QUEUEDB = require('../services/QUEUEDB');
 module.exports = async (request, reply) => {
   //
 
-  // If everything checks out, create a new job in the queue
-  const allJobs = await QUEUEDB.Job.find({});
+  const allJobs = await QUEUEDB.Job.find({}).toArray();
 
   console.log('allJobs', allJobs);
 
