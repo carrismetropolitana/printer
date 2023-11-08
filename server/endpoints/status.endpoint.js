8729432d371eacdca51a6abcdc820653352ec9d3
João de Vasconcelos <johnyvasconcelos@icloud.com>
2023-11-08 00:19:27 +0000

    Update status.endpoint.js

--- server/endpoints/status.endpoint.js
+++ server/endpoints/status.endpoint.js
@@ -16,12 +16,6 @@ module.exports = async (request, reply) => {
   // Simplify the requested job object
   const foundRequestedJobSimplified = { ...foundRequestedJob, owner_name: undefined, owner_email: undefined, gdpr_consent: undefined };
 
-  // If job is already expired (the file was deleted) return 410 Gone
-  if (foundRequestedJob.status === 'expired') return reply.code(410).send(foundRequestedJobSimplified);
-
-  // If job is not yet ready (the file is not yet available) return 204 No Content
-  if (foundRequestedJob.status !== 'ready') return reply.code(204).send(foundRequestedJobSimplified);
-
   // If the jobs is found, send the status object
   return reply.code(200).send(foundRequestedJobSimplified);
 
