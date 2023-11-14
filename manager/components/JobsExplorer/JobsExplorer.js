46c0ed827525bad54e5e11feb9baec02862dfd2d
João de Vasconcelos <johnyvasconcelos@icloud.com>
2023-11-14 00:58:27 +0000

    CORS working

--- manager/components/JobsExplorer/JobsExplorer.js
+++ manager/components/JobsExplorer/JobsExplorer.js
@@ -1,3 +1,5 @@
+'use client';
+
 /* * */
 
 import styles from './JobsExplorer.module.css';
@@ -9,12 +11,35 @@ import AppVersion from '@/components/AppVersion/AppVersion';
 /* * */
 
 export default function JobsExplorer() {
+  const handleClick = async () => {
+    try {
+      await fetch('https://printer.carrismetropolitana.pt/publish', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          owner_name: 'Teste',
+          owner_email: 'johnyvasconcelos@icloud.com',
+          owner_lang: 'pt',
+          gdpr_consent: true,
+          render_host: 'escolas.carrismetropolitana.pt',
+          render_path: '803239/render',
+          filename: 'Teste Nome PDF',
+        }),
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <JobsExplorerContextProvider>
       <div className={styles.container}>
         <JobsExplorerToolbar />
         <JobsExplorerTable />
         <AppVersion />
+        <div onClick={handleClick}>CLICK</div>
       </div>
     </JobsExplorerContextProvider>
   );
