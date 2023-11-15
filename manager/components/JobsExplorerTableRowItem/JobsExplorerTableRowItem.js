a43425dc280eceebec73345d690cf564f023f8a7
João de Vasconcelos <johnyvasconcelos@icloud.com>
2023-11-15 19:36:43 +0000

    CSS tweaks

--- manager/components/JobsExplorerTableRowItem/JobsExplorerTableRowItem.js
+++ manager/components/JobsExplorerTableRowItem/JobsExplorerTableRowItem.js
@@ -46,7 +46,7 @@ export default function JobsExplorerTableRowItem({ jobData }) {
         <div className={styles.column}>{jobData.render_path.substring(0, 30) || '(empty)'}</div>
         <div className={styles.column}>{jobData.render_format || '(empty)'}</div>
 
-        <div className={styles.column}>{jobData.filename || '(empty)'}</div>
+        <div className={styles.column}>{jobData.filename.substring(0, 30) || '(empty)'}</div>
       </JobsExplorerTableRowContainer>
     </div>
   );
