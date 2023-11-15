9116cc0950f46cc6aa20df1b9cbee44922847440
João de Vasconcelos <johnyvasconcelos@icloud.com>
2023-11-15 16:15:55 +0000

    Fix

--- notifier/services/SMTPSERVICE.js
+++ notifier/services/SMTPSERVICE.js
@@ -9,7 +9,6 @@ class SMTPSERVICE {
     this.transport = nodemailer.createTransport({
       host: process.env.EMAIL_SERVER_HOST,
       port: process.env.EMAIL_SERVER_PORT,
-      secure: true,
       auth: {
         user: process.env.EMAIL_SERVER_USER,
         pass: process.env.EMAIL_SERVER_PASSWORD,
